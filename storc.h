86bc7163d25ab06d3887d2ac1ebba43b28e9e442
cerebr4l <jsteele101010@gmail.com>
2024-06-25 20:22:31 -0400

    included libs, defined a few constants

--- storc.h
+++ storc.h
@@ -1,3 +1,13 @@
 /*
  * cerebr4l 2024
  */
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PXY "127.0.0.1"
+#define PXYPRT 9050
