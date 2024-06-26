93b20ee03aaa3c046f3a5ccd7bebc03c31fb9a08
cerebr4l <jsteele101010@gmail.com>
2024-06-26 00:25:23 -0400

    setup socket and localhost connection

--- storc.c
+++ storc.c
@@ -3,11 +3,18 @@
  */
 
 #include "storc.h"
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/_endian.h>
+#include <sys/socket.h>
 
 int main(int argc, char **argv) {
 
   char *host;
+  int s; // stream?, socket?, service?
   int16_t port;
+  struct sockaddr_in sock;
 
   if (argc < 3) {
     fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
@@ -16,4 +23,22 @@ int main(int argc, char **argv) {
 
   host = argv[1];
   port = atoi(argv[2]);
+
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    perror("socket");
+    return -1;
+  }
+  sock.sin_family = AF_INET;
+  sock.sin_port = htons(PROXYPORT);
+  sock.sin_addr.s_addr = inet_addr(PROXY);
+
+  if (connect(s, (struct sockaddr *)&sock, // typecasting the reference
+              sizeof(sock)) != 0) {
+    perror("connect");
+    return -1;
+  }
+  printf("connection successful\n");
+  close(s);
+  return 0;
 }
