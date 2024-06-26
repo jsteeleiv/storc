0afe4eb1abbdb58f8f0d4603eaef8849e10faa1d
cerebr4l <jsteele101010@gmail.com>
2024-06-25 20:22:45 -0400

    setup main structure

--- storc.c
+++ storc.c
@@ -2,9 +2,18 @@
  * cerebr4l 2024
  */
 
-#include "storc.h";
+#include "storc.h"
 
 int main(int argc, char **argv) {
 
-  // ip, port, etc.
+  char *host;
+  int16_t port;
+
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+    return -1;
+  }
+
+  host = argv[1];
+  port = atoi(argv[2]);
 }
